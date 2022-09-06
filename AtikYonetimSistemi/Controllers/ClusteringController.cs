d61fb8cf76794d136c2748ca7705dabe67b6652c
Begüm Yaprak <begmyaprak@gmail.com>
2022-09-06 13:58:06 +0300

    cluster added

---
+++ AtikYonetimSistemi/Controllers/ClusteringController.cs
@@ -0,0 +1,41 @@
+﻿using AtikYonetimSistemi.Context;
+using AtikYonetimSistemi.Models;
+using FluentNHibernate.Testing.Values;
+using Microsoft.AspNetCore.Mvc;
+using NHibernate.Engine.Query;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtikYonetimSistemi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ClusteringController : ControllerBase
+    {
+        private readonly IMapperSession _session;
+
+
+        public ClusteringController(IMapperSession session)
+        {
+            _session = session;
+
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<Container>> GetClustering(long id, int clusterNumber)
+        {
+            List<Container> vehicleContainerList = _session.Containers.Where(a => a.VehicleId == id).ToList();
+
+            var response = vehicleContainerList.Select((x, i) => new { Index = i, value = x })
+                .GroupBy(x => x.Index % clusterNumber)
+                .Select(x => x.Select(a => a.value).ToList())
+                .ToList();
+
+            return Ok(response);
+
+        }
+    }
+}
